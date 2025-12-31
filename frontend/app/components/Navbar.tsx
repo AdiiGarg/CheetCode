71607b97e48f14e518f097807a955d5ffa3f21a4
Adiii Garg <adiiigarg16@gmail.com>
2026-01-01 02:44:33 +0530

    -CheetCode v.10-

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
 
         {/* LOGO */}
-        <Link href="/" className="flex items-center gap-0 relative">
+        <Link href="/" className="flex items-center gap-0">
           <Image
             src="/logo.png"
             alt="CheetCode"
@@ -20,27 +20,9 @@ export default function Navbar() {
             height={32}
             priority
           />
-
-          {/* TEXT WRAPPER */}
-          <div className="relative flex flex-col leading-none">
-            {/* 2026 TAG */}
-            <span className="
-              absolute 
-              -top-3 
-              left-0 
-              text-[10px] 
-              tracking-[0.25em] 
-              text-zinc-400 
-              font-medium
-              select-none
-            ">
-              2026
-            </span>
-
-            <span className="text-lg font-semibold text-white">
-              CheetCode
-            </span>
-          </div>
+          <span className="text-lg font-semibold text-white">
+            CheetCode
+          </span>
         </Link>
 
         {/* RIGHT SIDE */}
